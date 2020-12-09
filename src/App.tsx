ca5e1c989535fe8ffed0cb5c5a602051e9ca9d59
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-08 20:16:56 -0600

    add infinite scroll boiler

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './Global.scss';
 import HomePage from './pages/HomePage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './components/Nav';
+import Nav from './components/nav/Nav';
+import InfiniteScrollPage from './pages/InfiniteScrollPage';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Switch>
         <Route exact path='/'>
           <HomePage />
+        </Route>
+        <Route exact path='/infinitescroll'>
+          <InfiniteScrollPage />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/InfiniteScrollPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InfiniteScroll from '../components/infinite-scroll/InfiniteScroll';
+
+const InfiniteScrollPage = () => {
+  return (
+    <>
+      <InfiniteScroll />
+    </>
+  );
+};
+
+export default InfiniteScrollPage;
