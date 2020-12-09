e372ec658b19d8ee8bcde2bb5c057d7d9e6aa0ff
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-08 20:32:33 -0600

    add search input

--- src/components/infinite-scroll/InfiniteScroll.tsx
+++ src/components/infinite-scroll/InfiniteScroll.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import './InfiniteScroll.styles.scss';
 
 const InfiniteScroll = () => {
-  return <div>Scrollin'</div>;
+  const [searchInput, setSearchInput] = React.useState('');
+  return (
+    <main className='scroll-container'>
+      <section className='search-bar'>
+        <label htmlFor='search'>Search Below</label>
+        <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          type='text'
+          autoFocus
+        />
+      </section>
+      <section className='image-gallery'></section>
+    </main>
+  );
 };
 
 export default InfiniteScroll;
