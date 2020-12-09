e7204c50e00e7d94ca77195942394036b898c6d1
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-08 19:50:22 -0600

    setup nav boiler and color theme (css variables)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import './App.css';
+import './Global.scss';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
-  return <h1>Yo</h1>;
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.styles.scss';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul className='nav-items'>
+        <Link to='/'>
+          <li>Home</li>
+        </Link>
+        <Link to='/infinitescroll'>
+          <li>Infinite Scroll</li>
+        </Link>
+        <Link to='/quiz'>
+          <li>Quiz</li>
+        </Link>
+        <Link to='/cart'>
+          <li>Cart</li>
+        </Link>
+        <Link to='/grudges'>
+          <li>Grudges</li>
+        </Link>
+        <Link to='/accordion'>
+          <li>Accordion</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
