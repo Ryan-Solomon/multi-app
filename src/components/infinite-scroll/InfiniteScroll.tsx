4f817cb487161f6d3e44b501acbf6f7bf920562e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-09 19:35:35 -0600

    store fetch return values in a use ref cache

--- src/components/infinite-scroll/InfiniteScroll.tsx
+++ src/components/infinite-scroll/InfiniteScroll.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './InfiniteScroll.styles.scss';
 import useUnsplash from './../../hooks/unsplash';
+import { Status } from '../../hooks/unsplash';
 
 const InfiniteScroll = () => {
   const [searchInput, setSearchInput] = React.useState('');
   const { photos, status } = useUnsplash('cars');
-  if (status === 'resolved') {
-    console.log(photos);
-  }
+
+  if (status === Status.rejected) return <h1>Something went wrong</h1>;
+  if (status === Status.pending) return <h1>loading</h1>;
+
   return (
     <main className='scroll-container'>
       <section className='search-bar'>
--- src/hooks/unsplash.tsx
+++ src/hooks/unsplash.tsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-enum Status {
+export enum Status {
   idle = 'idle',
   pending = 'pending',
   resolved = 'resolved',
   rejected = 'rejected',
 }
 
+type TPhoto = {
+  id: string;
+  likes: number;
+  urls: {
+    thumb: string;
+  };
+};
+
 const useUnsplash = (searchTerm: string) => {
-  const [photos, setPhotos] = useState([]);
+  const [photos, setPhotos] = useState<TPhoto[]>([]);
   const [status, setStatus] = useState<Status>(Status.idle);
+  const cache = useRef<{ [key: string]: TPhoto[] }>({});
 
   useEffect(() => {
     const getPhotos = async () => {
+      if (cache.current[searchTerm]) return;
       try {
+        console.log('fetching photos!');
         setStatus(Status.pending);
         const res = await fetch(
           `https://api.unsplash.com/search/photos?page=1&query=${searchTerm}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
         );
-        const data = await res.json();
-        setPhotos(data);
+        const { results } = await res.json();
+        setPhotos(results);
+        cache.current[searchTerm] = results;
         setStatus(Status.resolved);
       } catch (e) {
         console.warn(e);
