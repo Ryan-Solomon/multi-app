7e37d006b3146cefab534e6db334c74810801114
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-10 21:00:49 -0600

    close on infinit

--- src/hooks/unsplash.tsx
+++ src/hooks/unsplash.tsx
@@ -18,23 +18,23 @@ export type TPhoto = {
   };
 };
 
-const useUnsplash = (searchTerm: string) => {
+const useUnsplash = (searchTerm: string, page = 1) => {
   const [photos, setPhotos] = useState<TPhoto[]>([]);
   const [status, setStatus] = useState<Status>(Status.idle);
   const cache = useRef<{ [key: string]: TPhoto[] }>({});
 
   useEffect(() => {
     const getPhotos = async () => {
-      if (cache.current[searchTerm]) return;
+      if (cache.current[searchTerm + page]) return;
       try {
         console.log('fetching photos!');
         setStatus(Status.pending);
         const res = await fetch(
-          `https://api.unsplash.com/search/photos?page=1&query=${searchTerm}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+          `https://api.unsplash.com/search/photos?page=${page}&query=${searchTerm}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
         );
         const { results } = await res.json();
         setPhotos(results);
-        cache.current[searchTerm] = results;
+        cache.current[searchTerm + page] = results;
         setStatus(Status.resolved);
       } catch (e) {
         console.warn(e);
@@ -42,7 +42,7 @@ const useUnsplash = (searchTerm: string) => {
       }
     };
     getPhotos();
-  }, [searchTerm]);
+  }, [searchTerm, page]);
 
   return { photos, status };
 };
