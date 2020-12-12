36f6df1889e3bfe2fd12bad8858eaf957f05ac36
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-11 19:44:14 -0600

    inf scroll

--- src/components/infinite-scroll/InfiniteScroll.tsx
+++ src/components/infinite-scroll/InfiniteScroll.tsx
@@ -9,33 +9,6 @@ const InfiniteScroll = () => {
   const [page, setPage] = React.useState(1);
   const [searchTerm, setSearchTerm] = React.useState('cars');
   const { photos, status } = useUnsplash(searchTerm, page);
-  const bottomElementRef = React.useRef<HTMLDivElement | null>(null);
-  console.log(page);
-
-  const loadMore = React.useCallback((entries) => {
-    const target = entries[0];
-    if (target.isIntersection) {
-      setPage((p) => p + 1);
-    }
-  }, []);
-
-  React.useEffect(() => {
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0.25,
-    };
-
-    const observer = new IntersectionObserver(loadMore, options);
-
-    if (bottomElementRef && bottomElementRef.current) {
-      observer.observe(bottomElementRef.current);
-    }
-
-    const refValue = bottomElementRef.current;
-
-    return () => observer.unobserve(refValue!);
-  }, [bottomElementRef, loadMore]);
 
   if (status === Status.rejected) return <h1>Something went wrong</h1>;
   if (status === Status.pending) return <h1>loading</h1>;
@@ -58,7 +31,6 @@ const InfiniteScroll = () => {
             return <PhotoBox key={photo.id} photo={photo} />;
           })}
       </section>
-      <div ref={bottomElementRef}>load more</div>
     </main>
   );
 };
