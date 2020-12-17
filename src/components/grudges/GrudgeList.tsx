03f06ae65edad39e13e5143fd1c007807a53475e
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-16 20:22:40 -0600

    setup grudges reducer

--- src/components/grudges/GrudgeList.tsx
+++ src/components/grudges/GrudgeList.tsx
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+type TGrudge = {
+  text: string;
+  id: number;
+};
+
+type TState = {
+  grudges: TGrudge[];
+};
+
+type TAction =
+  | {
+      type: 'ADD';
+      payload: TGrudge;
+    }
+  | {
+      type: 'DELETE';
+      payload: number;
+    }
+  | {
+      type: 'CLEAR';
+    };
+
+const initialState: TState = {
+  grudges: [],
+};
+
+function reducer(state: TState, action: TAction): TState {
+  switch (action.type) {
+    case 'ADD':
+      return { grudges: [...state.grudges, action.payload] };
+    case 'DELETE':
+      return {
+        grudges: [...state.grudges.filter(({ id }) => id !== action.payload)],
+      };
+    case 'CLEAR':
+      return { grudges: [] };
+    default:
+      throw new Error('Action not supported');
+  }
+}
 
 const GrudgeList = () => {
+  const [grudges, setGrudges] = useReducer(reducer, initialState);
   return <div>grudges</div>;
 };
 
