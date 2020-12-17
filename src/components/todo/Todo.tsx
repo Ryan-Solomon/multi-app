9116ebfe1f333284ec68234761de6307b29e35f3
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-16 20:10:43 -0600

    exciting. add todos

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import './Todo.styles.scss';
 
+type TTodos = {
+  todoText: string;
+  id: number;
+};
+
 const Todo = () => {
+  const [input, setInput] = React.useState('');
+  const [todos, setTodos] = React.useState<TTodos[]>([]);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setTodos((todos) => [...todos, { todoText: input, id: Math.random() }]);
+    setInput('');
+  };
+
+  const removeTodo = (id1: number) => {
+    setTodos((todos) => todos.filter(({ id }) => id !== id1));
+  };
+
   return (
     <div className='todo-container'>
       <header>
         <h1>stuff i need to do</h1>
       </header>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='todo'>Add Todo</label>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type='text'
+        />
+        <button type='submit'>Add Todo</button>
+      </form>
+      <section className='todos'>
+        {todos.length > 0 &&
+          todos.map(({ todoText, id }) => {
+            return (
+              <>
+                <h2 key={id}>{todoText}</h2>
+                <button onClick={() => removeTodo(id)}>X</button>
+              </>
+            );
+          })}
+      </section>
     </div>
   );
 };
